6d7b8f552fd980ea871b9720deca1d9c2fd6e63e
dsayal <157721088+dsayal@users.noreply.github.com>
2024-08-03 13:06:42 -0400

    update import packages

--- src/main.py
+++ src/main.py
@@ -1,26 +1,25 @@
 '''
 You will run this problem set from main.py, so set things up accordingly
 '''
-
 import pandas as pd
-import etl
-import preprocessing
-import logistic_regression
-import decision_tree
-import calibration_plot
+from part1_etl import etl
+from part2_preprocessing import preprocessing
+from part3_logistic_regression import logistic_regression
+from part4_decision_tree import decision_tree
+from part5_calibration_plot import calibration_plot
 
 def main():
-    # PART 1: Instanciate etl, saving the two datasets in `./data/`
+    # PART 1: Instantiate ETL, saving the two datasets in `./data/`
     etl()
     
     # PART 2: Call functions from preprocessing
     df_arrests = preprocessing()
     
     # PART 3: Call functions from logistic_regression
-    logistic_regression(df_arrests)
+    df_arrests = logistic_regression(df_arrests)
     
     # PART 4: Call functions from decision_tree
-    decision_tree(df_arrests)
+    df_arrests = decision_tree(df_arrests)
     
     # PART 5: Call functions from calibration_plot
     calibration_plot(df_arrests)
