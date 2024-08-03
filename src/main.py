115933a0ce95284c1cb0cecf279f1a3564bbbd92
dsayal <157721088+dsayal@users.noreply.github.com>
2024-08-03 13:42:58 -0400

    update packages

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 '''
 You will run this problem set from main.py, so set things up accordingly
 '''
-import pandas as pd
-from part1_etl import etl
-from part2_preprocessing import preprocessing
-from part3_logistic_regression import logistic_regression
-from part4_decision_tree import decision_tree
-from part5_calibration_plot import calibration_plot
+import etl
+import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.calibration import calibration_curve
+
 
 def main():
     # PART 1: Instantiate ETL, saving the two datasets in `./data/`
@@ -16,13 +16,13 @@ def main():
     df_arrests = preprocessing()
     
     # PART 3: Call functions from logistic_regression
-    df_arrests = logistic_regression(df_arrests)
+    df_arrests = LogisticRegression(df_arrests)
     
     # PART 4: Call functions from decision_tree
-    df_arrests = decision_tree(df_arrests)
+    df_arrests = DecisionTreeClassifier(df_arrests)
     
     # PART 5: Call functions from calibration_plot
-    calibration_plot(df_arrests)
+    calibration_curve(df_arrests)
 
 if __name__ == "__main__":
     main()
